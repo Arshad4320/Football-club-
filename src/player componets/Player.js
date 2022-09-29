1a237d7d1eea286505f9564f3f03e5e12ea103a2
Arshad Ullah <mdarshadullah835@gmail.com>
2022-09-29 19:51:35 +0600

    add to The card button

--- src/player componets/Player.js
+++ src/player componets/Player.js
@@ -27,6 +27,20 @@ const Player = () => {
                     <SideComponent sideCard={time}></SideComponent>
                 </div>
             </div>
+            <div className="question-container container">
+                <div className="question-items">
+                    <h4>How Does React Works?</h4>
+                    <p>React has some core features that make it stand out from other JavaScript libraries. The following sections will introduce you to these features and explain how they contribute to mobile and web application development.</p>
+                </div>
+                <div className="question-items">
+                    <h4>What is difference between props and state in react?</h4>
+                    <p>We can pass the data between the components using Props and State. So, let us know how we can pass the data using props and state and understand the difference between the two.</p>
+                </div>
+                <div className="question-items">
+                    <h4>How use effect without api load working in react?</h4>
+                    <p>In React development, web application programming interfaces (APIs) are an integral part of single-page application (SPA) designs. APIs are the primary way for applications to programmatically communicate with servers to provide users with real-time data and to save user changes. In React applications, you will use APIs to load user preferences, display user information, fetch configuration or security information, and save application state changes.</p>
+                </div>
+            </div>
         </div>
     );
 };
